7d466021f36ec61ae118a211bf909594153923b7
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2022-01-27 16:05:37 +0900

    해시를 이용한 배열 탐색
    마라톤 참가자 명단, 완주자 명단을 비교하여 완주하지 못한 사람 탐색

--- src/main/java/hash/Marathon.java
+++ src/main/java/hash/Marathon.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Marathon {
-        public String solution(String[] participant, String[] completion) {
+
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
         //hashmap 에 완주자 명단 넣기
         HashMap<String, Integer> completiontHashMap = new HashMap<String, Integer>();
         for (int i = 0; i < completion.length; i++) {
             //동명이인 value값으로 카운트
             //Hashmap.getOrDefault : 찾는 키가 존재한다면 찾는 키의 값을 반환하고, 없다면 기본값을 반환하는 메서드
-            completiontHashMap.put(completion[i], completiontHashMap.getOrDefault(completion[i],0) + 1);
+            completiontHashMap.put(completion[i],
+                completiontHashMap.getOrDefault(completion[i], 0) + 1);
         }
         //hashmap 에서 완주하지 못한 인원 탐색
         for (int i = 0; i < participant.length; i++) {
