9299d33eecd83ddaefd964ba442cce884345eb92
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-04-22 04:50:57 +0900

    문제 해결중
    genresRank 오름차순으로 졍렬해서 put하기
    answer 크기 정확히 세서 넣기
    (music play 횟수같을때는 어떻게 동작하는지 확인하지 못함)
    나머진 잘됨
    collection.sort, compare compareTo 확실히 공부할것

--- src/main/java/hash/BestAlbum.java
+++ src/main/java/hash/BestAlbum.java
@@ -38,8 +38,8 @@ package hash;
     3. 정렬된 장르 순위별 곡 2개씩 뽑아서 넣기
 
     map 구조
-    Map1<장르, Map2<고유번호 , 플레이 횟수>>
-    Map3<장르, 총 플레이 회수>
+    Map1<장르, TreeMap2<플레이 횟수 , 고유번호>>
+    TreeMap3<총 플레이 회수, 장르>
 
     1. Map2의 value 기준으로 고유번호 정렬 (TreeMap과 Comparator 를 이용)
     2. Map3<장르, 총 플레이 회수> value 기준 정렬 (TreeMap과 Comparator를 이용)
@@ -55,27 +55,79 @@ package hash;
     이중맵과 hashMap value 값기준 정렬 하는법
  */
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
 
 
 public class BestAlbum {
-    //TreeMap을 통해 이중Map 의 value 정렬하기
     //["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
     public int[] solution(String[] genres, int[] plays) {
         //1.Map1<장르, treeMap<플레이 횟수, 고유번호>> 정렬하기
-        int[] answer = {};
+        int[] answer = new int[genres.length * 2];
+        int answerIndex = 0;
+        Map<String, Map<Integer, Integer>> genresMap = new HashMap<>();
+        Map<String, Integer> genresRank = new TreeMap<>();
+
+        //Task1. TreeMap2 만들어서 rankGenres에 넣기
+        //Task2. TreeMap3 만들어서 장르랭크 만들기
+        for (int i = 0; i < genres.length; i++) {
+            //TreeMap2 만들기
+            //해당 장르를 처음 저장한다면 장르에 맞는 musicRank 만들어서 넣어주기
+            if (!genresMap.containsKey(genres[i])) {
+                Map<Integer, Integer> musicRank = new HashMap<>();
+                genresMap.put(genres[i], musicRank);
+            }
+            genresMap.get(genres[i]).put(i, plays[i]);
+
+            //TreeMap3 만들어서 장르랭크 넣기
+            genresRank.put(genres[i], genresRank.getOrDefault(genres[i], 0) + plays[i]);
+        }
+
+        //2. 장르별 수록곡 순위 정렬하고 (musicRank의 value값 기준 정렬) answer에 고유번호넣기
+        Set<String> keySet = genresRank.keySet();
+        Iterator<String> keyIterator = keySet.iterator();
+        while (keyIterator.hasNext()) {
+            String key = keyIterator.next();
+            //1등 장르의 <고유번호, 플레이횟수> Map을 가져오기
+            Map<Integer, Integer> musicMap = genresMap.get(key);
+
+            //해당 Map을 value값 기준 정렬
+            List<Entry<Integer, Integer>> list_entries = new ArrayList<Entry<Integer, Integer>>(musicMap.entrySet());
+
+            Collections.sort(list_entries, new Comparator<Entry<Integer, Integer>>() {
+                public int compare(Entry<Integer, Integer> obj1, Entry<Integer, Integer> obj2) {
+                    //오름 차순 정렬
+                    //return obj1.getValue().compareTo(obj2.getValue());
+                    //내림 차순 정렬
+                    if (obj2.getValue() == obj1.getValue()) {
+                        if (obj2.getKey() > obj1.getKey()) {
+                            return -1;
+                        } else {
+                            return 1;
+                        }
+                    }
+                    // obj1 - obj2 를했을때 오름차순으로 정렬된다
+                    // obj2 - obj1 을했을때 내림차순으로 정렬된다
+                    return obj2.getValue().compareTo(obj1.getValue());
+                }
+            });
+
+            //만약 장르에 수록된 곡이 2개 미만이라면
+            if (list_entries.size() < 2) {
+                answer[answerIndex++] = list_entries.get(0).getKey();
+            } else {
+                answer[answerIndex++] = list_entries.get(0).getKey();
+                answer[answerIndex++] = list_entries.get(1).getKey();
+            }
+        }
         return answer;
     }
 
 }
+
+
+genresRank 오름차순으로 졍렬해서 put하기
+answer 크기 정확히 세서 넣기
+(music play 횟수같을때는 어떻게 동작하는지 확인하지 못함)
+나머진 잘됨
+collection.sort, compare compareTo 확실히 공부할것
